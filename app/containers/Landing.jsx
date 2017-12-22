165035440dcaa595cc31994cadc81ea28c06d317
Karan Kotwal <kotwalkaran64@gmail.com>
2017-12-22 17:31:38 -0500

    Adding video to landing page

--- app/containers/Landing.jsx
+++ app/containers/Landing.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classNames from 'classnames/bind';
 import styles from '../css/components/landing.css';
-
+import video from '../video/Ma-Vibes.mp4';
+import videoImg from '../images/Ma-Vibes.jpg';
 const cx = classNames.bind(styles);
 
-const Landing = () => {
-  return (
-    <div>
-      <p>Welcome to the Landing page</p>
-    </div>
-  );
-};
+class Landing extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={cx('landing')}>
+        <div className={cx('hero-container')}>
+          <video autoPlay loop>
+            <source src={video} type="video/mp4" />
+          </video>
+          <div className={cx('hidden')}>
+            <img src={videoImg} alt="" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Landing;
--- webpack/rules/index.js
+++ webpack/rules/index.js
@@ -1,11 +1,13 @@
 const image = require('./image');
 const javascript = require('./javascript');
 const css = require('./css');
+const video = require('./video');
 
 module.exports = ({ production = false, browser = false } = {}) => (
   [
     javascript({ production, browser }),
     css({ production, browser }),
-    image()
+    image(),
+    video()
   ]
 );
